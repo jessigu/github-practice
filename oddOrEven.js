7733fe6f8b495b972323a41a60e35ca4039f30fc
Jess Guttenberg <jessguttenberg@Admins-MBP.localdomain>
2019-02-15 11:40:46 -0800

    added func to file

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,7 +1,11 @@
 // Update the following function to take an argument and output 'Even' if the argument is an even number or 'Odd' if the number is an odd number.
 
-function oddOrEven(){
-    console.log('result')
+function oddOrEven(num){
+  if (num % 2 === 0) {
+    return "Even"
+  } else {
+    return "Odd"
+  }
 }
 
 oddOrEven(4)
